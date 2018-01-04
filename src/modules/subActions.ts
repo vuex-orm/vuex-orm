25dbb306085366bb4edca2b7a1b3d40af8da6403
Kia King Ishii <kia.king.08@gmail.com>
2018-01-05 00:29:04 +0900

    Adjust update condition a bit

--- src/modules/subActions.ts
+++ src/modules/subActions.ts
@@ -23,16 +23,10 @@ export default {
    * Update data in the store.
    */
   update ({ commit, state }, payload) {
-    let where = payload.where
-    let data = payload.data
+    const where = payload.where
+    const data = payload.data
 
-    if (where === undefined && data === undefined) {
-      commit(`${state.$connection}/update`, { entity: state.$name, data: payload }, { root: true })
-
-      return
-    }
-
-    if (typeof data !== 'object') {
+    if (where === undefined || data === undefined) {
       commit(`${state.$connection}/update`, { entity: state.$name, data: payload }, { root: true })
 
       return
