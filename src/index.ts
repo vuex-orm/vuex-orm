044c185d7b67bfcf862e87c13ab17606bb146170
Kia King Ishii <kia.king.08@gmail.com>
2018-01-12 22:13:45 +0900

    Expose Repo and Query to the user

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,32 @@ import install, { Install } from './store/install'
 import use, { Use } from './plugins/use'
 import Database from './Database'
 import Model from './Model'
+import Repo from './Repo'
+import Query from './Query'
 
 export interface VuexORM {
   install: Install
   use: Use
   Database: typeof Database
   Model: typeof Model
+  Repo: typeof Repo
+  Query: typeof Query
 }
 
 export default {
   install,
   use,
   Database,
-  Model
+  Model,
+  Repo,
+  Query
 } as VuexORM
 
 export {
   install,
   use,
   Database,
-  Model
+  Model,
+  Repo,
+  Query
 }
