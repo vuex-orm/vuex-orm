6355eca54a26c66cdcafbfa0423d5e13d558bc2b
Kia King Ishii <kia.king.08@gmail.com>
2018-02-14 01:18:06 +0900

    Remove unused constructor

--- src/repo/types/Increment.ts
+++ src/repo/types/Increment.ts
@@ -2,12 +2,5 @@ export default class Increment {
   /**
    * The initial count to start incrementing.
    */
-  value: number
-
-  /**
-   * Create a new increment instance.
-   */
-  constructor (value: number = 1) {
-    this.value = value
-  }
+  value: number = 1
 }
