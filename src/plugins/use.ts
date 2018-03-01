1e17bf95917b34b2c4f8cef5b5505b5f75fcb030
Kia King Ishii <kia.king.08@gmail.com>
2018-03-02 00:07:01 +0900

    Expose attributes

--- src/plugins/use.ts
+++ src/plugins/use.ts
@@ -1,6 +1,21 @@
 import Model from '../model/Model'
 import Repo from '../repo/Repo'
 import Query from '../repo/Query'
+import Type from '../attributes/types/Type'
+import Attr from '../attributes/types/Attr'
+import Increment from '../attributes/types/Increment'
+import Relation from '../attributes/relations/Relation'
+import HasOne from '../attributes/relations/HasOne'
+import BelongsTo from '../attributes/relations/BelongsTo'
+import HasMany from '../attributes/relations/HasMany'
+import HasManyBy from '../attributes/relations/HasManyBy'
+import BelongsToMany from '../attributes/relations/BelongsToMany'
+import HasManyThrough from '../attributes/relations/HasManyThrough'
+import MorphTo from '../attributes/relations/MorphTo'
+import MorphOne from '../attributes/relations/MorphOne'
+import MorphMany from '../attributes/relations/MorphMany'
+import MorphToMany from '../attributes/relations/MorphToMany'
+import MorphedByMany from '../attributes/relations/MorphedByMany'
 import rootGetters, { RootGetters } from '../modules/rootGetters'
 import subGetters, { SubGetters } from '../modules/subGetters'
 import rootActions, { RootActions } from '../modules/rootActions'
@@ -11,6 +26,21 @@ export interface Components {
   Model: typeof Model
   Repo: typeof Repo
   Query: typeof Query
+  Type: typeof Type
+  Attr: typeof Attr
+  Increment: typeof Increment
+  Relation: typeof Relation
+  HasOne: typeof HasOne
+  BelongsTo: typeof BelongsTo
+  HasMany: typeof HasMany
+  HasManyBy: typeof HasManyBy
+  BelongsToMany: typeof BelongsToMany
+  HasManyThrough: typeof HasManyThrough
+  MorphTo: typeof MorphTo
+  MorphOne: typeof MorphOne
+  MorphMany: typeof MorphMany
+  MorphToMany: typeof MorphToMany
+  MorphedByMany: typeof MorphedByMany
   rootGetters: RootGetters
   subGetters: SubGetters
   rootActions: RootActions
@@ -34,6 +64,21 @@ export default function (plugin: Plugin, options: Options = {}): void {
     Model,
     Repo,
     Query,
+    Type,
+    Attr,
+    Increment,
+    Relation,
+    HasOne,
+    BelongsTo,
+    HasMany,
+    HasManyBy,
+    BelongsToMany,
+    HasManyThrough,
+    MorphTo,
+    MorphOne,
+    MorphMany,
+    MorphToMany,
+    MorphedByMany,
     rootGetters,
     subGetters,
     rootActions,
