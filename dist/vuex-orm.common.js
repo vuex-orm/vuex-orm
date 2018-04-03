de86d247c4b8d0e68febd35525fd7f2b91504a80
Kia King Ishii <kia.king.08@gmail.com>
2018-04-04 01:14:57 +0900

    [Build] 0.24.1

--- dist/vuex-orm.common.js
+++ dist/vuex-orm.common.js
@@ -2452,23 +2452,6 @@ var Schema = /** @class */ (function () {
     return Schema;
 }());
 
-var PivotCreator = /** @class */ (function () {
-    function PivotCreator() {
-    }
-    PivotCreator.create = function (data, Query) {
-        Object.keys(data).forEach(function (key) {
-            var model = Query.getModel(key);
-            if (model.hasPivotFields()) {
-                Utils.forOwn(model.pivotFields(), function (field) {
-                    Utils.forOwn(field, function (attr) { attr.createPivots(model, data); });
-                });
-            }
-        });
-        return data;
-    };
-    return PivotCreator;
-}());
-
 var Normalizer = /** @class */ (function () {
     function Normalizer() {
     }
@@ -2480,10 +2463,30 @@ var Normalizer = /** @class */ (function () {
             return {};
         }
         var schema = Array.isArray(data) ? Schema.many(Query.model) : Schema.one(Query.model);
-        var normalizedData = src_2(data, schema).entities;
-        return PivotCreator.create(normalizedData, Query);
+        return src_2(data, schema).entities;
     };
     return Normalizer;
+}());
+
+var PivotCreator = /** @class */ (function () {
+    function PivotCreator() {
+    }
+    /**
+     * Create an intermediate entity if the data contains any entities that
+     * require it for example `belongsTo` or `morphMany`.
+     */
+    PivotCreator.process = function (data, Query) {
+        Object.keys(data).forEach(function (key) {
+            var model = Query.getModel(key);
+            if (model.hasPivotFields()) {
+                Utils.forOwn(model.pivotFields(), function (field) {
+                    Utils.forOwn(field, function (attr) { attr.createPivots(model, data); });
+                });
+            }
+        });
+        return data;
+    };
+    return PivotCreator;
 }());
 
 var Incrementer = /** @class */ (function () {
@@ -2610,6 +2613,7 @@ var Data = /** @class */ (function () {
      */
     Data.normalize = function (data, query) {
         data = Normalizer.process(data, query);
+        data = PivotCreator.process(data, query);
         data = Incrementer.process(data, query);
         data = Attacher.process(data, query);
         data = IdFixer.process(data, query);
@@ -2860,8 +2864,9 @@ var Query = /** @class */ (function () {
         return Container.connection(state.$name).modules();
     };
     /**
-     * Save the given data to the state. This will replace any existing
-     * data in the state.
+     * Save new data to the state. It will remove all existing data in the
+     * state. If you want to keep existing data while saving new data,
+     * use `insert` instead.
      */
     Query.create = function (state, entity, data, options) {
         return (new this(state, entity)).create(data, options);
@@ -3045,8 +3050,9 @@ var Query = /** @class */ (function () {
         return this;
     };
     /**
-     * Save the given data to the state. It will replace any existing
-     * data in the state.
+     * Save new data to the state. It will remove all existing data in the
+     * state. If you want to keep existing data while saving new data,
+     * use `insert` instead.
      */
     Query.prototype.create = function (data, options) {
         return this.persist(data, 'create', options);
@@ -4338,55 +4344,17 @@ var subGetters = {
     }; }
 };
 
-var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [0, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var rootActions = {
     /**
-     * Save the given data to the state. This will replace any existing
-     * data in the state.
+     * Save new data to the state. It will remove all existing data in the
+     * state. If you want to keep existing data while saving new data,
+     * use `insert` instead.
      */
     create: function (context, _a) {
         var entity = _a.entity, data = _a.data, create = _a.create, insert = _a.insert, update = _a.update, insertOrUpdate = _a.insertOrUpdate;
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_b) {
-                return [2 /*return*/, (new Query(context.state, entity))
-                        .setActionContext(context)
-                        .create(data, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate })];
-            });
-        });
+        return (new Query(context.state, entity))
+            .setActionContext(context)
+            .create(data, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate });
     },
     /**
      * Insert given data to the state. Unlike `create`, this method will not
@@ -4395,26 +4363,18 @@ var rootActions = {
      */
     insert: function (context, _a) {
         var entity = _a.entity, data = _a.data, create = _a.create, insert = _a.insert, update = _a.update, insertOrUpdate = _a.insertOrUpdate;
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_b) {
-                return [2 /*return*/, (new Query(context.state, entity))
-                        .setActionContext(context)
-                        .insert(data, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate })];
-            });
-        });
+        return (new Query(context.state, entity))
+            .setActionContext(context)
+            .insert(data, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate });
     },
     /**
      * Update data in the store.
      */
     update: function (context, _a) {
         var entity = _a.entity, where = _a.where, data = _a.data, create = _a.create, insert = _a.insert, update = _a.update, insertOrUpdate = _a.insertOrUpdate;
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_b) {
-                return [2 /*return*/, (new Query(context.state, entity))
-                        .setActionContext(context)
-                        .update(data, where, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate })];
-            });
-        });
+        return (new Query(context.state, entity))
+            .setActionContext(context)
+            .update(data, where, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate });
     },
     /**
      * Insert or update given data to the state. Unlike `insert`, this method
@@ -4453,8 +4413,9 @@ var __assign$9 = (undefined && undefined.__assign) || Object.assign || function(
 };
 var subActions = {
     /**
-     * Save the given data to the state. This will replace any existing
-     * data in the state.
+     * Save new data to the state. It will remove all existing data in the
+     * state. If you want to keep existing data while saving new data,
+     * use `insert` instead.
      */
     create: function (_a, payload) {
         var dispatch = _a.dispatch, state = _a.state;
@@ -4495,23 +4456,22 @@ var subActions = {
     delete: function (_a, condition) {
         var dispatch = _a.dispatch, state = _a.state;
         var where = typeof condition === 'object' ? condition.where : condition;
-        return dispatch(state.$connection + "/insertOrUpdate", { entity: state.$name, where: where }, { root: true });
+        return dispatch(state.$connection + "/delete", { entity: state.$name, where: where }, { root: true });
     },
     /**
      * Delete all data from the store.
      */
     deleteAll: function (_a) {
-        var commit = _a.commit, state = _a.state;
-        commit(state.$connection + "/deleteAll", {
-            entity: state.$name
-        }, { root: true });
+        var dispatch = _a.dispatch, state = _a.state;
+        dispatch(state.$connection + "/deleteAll", { entity: state.$name }, { root: true });
     }
 };
 
 var mutations = {
     /**
-     * Save the given data to the state. This will replace any existing
-     * data in the state.
+     * Save new data to the state. It will remove all existing data in the
+     * state. If you want to keep existing data while saving new data,
+     * use `insert` instead.
      */
     create: function (state, _a) {
         var entity = _a.entity, data = _a.data, create = _a.create, insert = _a.insert, update = _a.update, insertOrUpdate = _a.insertOrUpdate;
@@ -4560,9 +4520,8 @@ var mutations = {
      * the submitted data with the same primary key.
      */
     insertOrUpdate: function (state, _a) {
-        var entity = _a.entity, data = _a.data, create = _a.create, done = _a.done;
-        var result = Query.insertOrUpdate(state, entity, data, create);
-        done && done(result);
+        var entity = _a.entity, data = _a.data, create = _a.create;
+        Query.insertOrUpdate(state, entity, data, create);
     },
     /**
      * Delete data from the store.
--- dist/vuex-orm.js
+++ dist/vuex-orm.js
@@ -2456,23 +2456,6 @@
         return Schema;
     }());
 
-    var PivotCreator = /** @class */ (function () {
-        function PivotCreator() {
-        }
-        PivotCreator.create = function (data, Query) {
-            Object.keys(data).forEach(function (key) {
-                var model = Query.getModel(key);
-                if (model.hasPivotFields()) {
-                    Utils.forOwn(model.pivotFields(), function (field) {
-                        Utils.forOwn(field, function (attr) { attr.createPivots(model, data); });
-                    });
-                }
-            });
-            return data;
-        };
-        return PivotCreator;
-    }());
-
     var Normalizer = /** @class */ (function () {
         function Normalizer() {
         }
@@ -2484,10 +2467,30 @@
                 return {};
             }
             var schema = Array.isArray(data) ? Schema.many(Query.model) : Schema.one(Query.model);
-            var normalizedData = src_2(data, schema).entities;
-            return PivotCreator.create(normalizedData, Query);
+            return src_2(data, schema).entities;
         };
         return Normalizer;
+    }());
+
+    var PivotCreator = /** @class */ (function () {
+        function PivotCreator() {
+        }
+        /**
+         * Create an intermediate entity if the data contains any entities that
+         * require it for example `belongsTo` or `morphMany`.
+         */
+        PivotCreator.process = function (data, Query) {
+            Object.keys(data).forEach(function (key) {
+                var model = Query.getModel(key);
+                if (model.hasPivotFields()) {
+                    Utils.forOwn(model.pivotFields(), function (field) {
+                        Utils.forOwn(field, function (attr) { attr.createPivots(model, data); });
+                    });
+                }
+            });
+            return data;
+        };
+        return PivotCreator;
     }());
 
     var Incrementer = /** @class */ (function () {
@@ -2614,6 +2617,7 @@
          */
         Data.normalize = function (data, query) {
             data = Normalizer.process(data, query);
+            data = PivotCreator.process(data, query);
             data = Incrementer.process(data, query);
             data = Attacher.process(data, query);
             data = IdFixer.process(data, query);
@@ -2864,8 +2868,9 @@
             return Container.connection(state.$name).modules();
         };
         /**
-         * Save the given data to the state. This will replace any existing
-         * data in the state.
+         * Save new data to the state. It will remove all existing data in the
+         * state. If you want to keep existing data while saving new data,
+         * use `insert` instead.
          */
         Query.create = function (state, entity, data, options) {
             return (new this(state, entity)).create(data, options);
@@ -3049,8 +3054,9 @@
             return this;
         };
         /**
-         * Save the given data to the state. It will replace any existing
-         * data in the state.
+         * Save new data to the state. It will remove all existing data in the
+         * state. If you want to keep existing data while saving new data,
+         * use `insert` instead.
          */
         Query.prototype.create = function (data, options) {
             return this.persist(data, 'create', options);
@@ -4342,55 +4348,17 @@
         }; }
     };
 
-    var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
-        return new (P || (P = Promise))(function (resolve, reject) {
-            function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-            function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-            function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-            step((generator = generator.apply(thisArg, _arguments || [])).next());
-        });
-    };
-    var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
-        var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-        return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-        function verb(n) { return function (v) { return step([n, v]); }; }
-        function step(op) {
-            if (f) throw new TypeError("Generator is already executing.");
-            while (_) try {
-                if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-                if (y = 0, t) op = [0, t.value];
-                switch (op[0]) {
-                    case 0: case 1: t = op; break;
-                    case 4: _.label++; return { value: op[1], done: false };
-                    case 5: _.label++; y = op[1]; op = [0]; continue;
-                    case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                    default:
-                        if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                        if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                        if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                        if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                        if (t[2]) _.ops.pop();
-                        _.trys.pop(); continue;
-                }
-                op = body.call(thisArg, _);
-            } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-            if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-        }
-    };
     var rootActions = {
         /**
-         * Save the given data to the state. This will replace any existing
-         * data in the state.
+         * Save new data to the state. It will remove all existing data in the
+         * state. If you want to keep existing data while saving new data,
+         * use `insert` instead.
          */
         create: function (context, _a) {
             var entity = _a.entity, data = _a.data, create = _a.create, insert = _a.insert, update = _a.update, insertOrUpdate = _a.insertOrUpdate;
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_b) {
-                    return [2 /*return*/, (new Query(context.state, entity))
-                            .setActionContext(context)
-                            .create(data, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate })];
-                });
-            });
+            return (new Query(context.state, entity))
+                .setActionContext(context)
+                .create(data, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate });
         },
         /**
          * Insert given data to the state. Unlike `create`, this method will not
@@ -4399,26 +4367,18 @@
          */
         insert: function (context, _a) {
             var entity = _a.entity, data = _a.data, create = _a.create, insert = _a.insert, update = _a.update, insertOrUpdate = _a.insertOrUpdate;
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_b) {
-                    return [2 /*return*/, (new Query(context.state, entity))
-                            .setActionContext(context)
-                            .insert(data, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate })];
-                });
-            });
+            return (new Query(context.state, entity))
+                .setActionContext(context)
+                .insert(data, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate });
         },
         /**
          * Update data in the store.
          */
         update: function (context, _a) {
             var entity = _a.entity, where = _a.where, data = _a.data, create = _a.create, insert = _a.insert, update = _a.update, insertOrUpdate = _a.insertOrUpdate;
-            return __awaiter(this, void 0, void 0, function () {
-                return __generator(this, function (_b) {
-                    return [2 /*return*/, (new Query(context.state, entity))
-                            .setActionContext(context)
-                            .update(data, where, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate })];
-                });
-            });
+            return (new Query(context.state, entity))
+                .setActionContext(context)
+                .update(data, where, { create: create, insert: insert, update: update, insertOrUpdate: insertOrUpdate });
         },
         /**
          * Insert or update given data to the state. Unlike `insert`, this method
@@ -4457,8 +4417,9 @@
     };
     var subActions = {
         /**
-         * Save the given data to the state. This will replace any existing
-         * data in the state.
+         * Save new data to the state. It will remove all existing data in the
+         * state. If you want to keep existing data while saving new data,
+         * use `insert` instead.
          */
         create: function (_a, payload) {
             var dispatch = _a.dispatch, state = _a.state;
@@ -4499,23 +4460,22 @@
         delete: function (_a, condition) {
             var dispatch = _a.dispatch, state = _a.state;
             var where = typeof condition === 'object' ? condition.where : condition;
-            return dispatch(state.$connection + "/insertOrUpdate", { entity: state.$name, where: where }, { root: true });
+            return dispatch(state.$connection + "/delete", { entity: state.$name, where: where }, { root: true });
         },
         /**
          * Delete all data from the store.
          */
         deleteAll: function (_a) {
-            var commit = _a.commit, state = _a.state;
-            commit(state.$connection + "/deleteAll", {
-                entity: state.$name
-            }, { root: true });
+            var dispatch = _a.dispatch, state = _a.state;
+            dispatch(state.$connection + "/deleteAll", { entity: state.$name }, { root: true });
         }
     };
 
     var mutations = {
         /**
-         * Save the given data to the state. This will replace any existing
-         * data in the state.
+         * Save new data to the state. It will remove all existing data in the
+         * state. If you want to keep existing data while saving new data,
+         * use `insert` instead.
          */
         create: function (state, _a) {
             var entity = _a.entity, data = _a.data, create = _a.create, insert = _a.insert, update = _a.update, insertOrUpdate = _a.insertOrUpdate;
@@ -4564,9 +4524,8 @@
          * the submitted data with the same primary key.
          */
         insertOrUpdate: function (state, _a) {
-            var entity = _a.entity, data = _a.data, create = _a.create, done = _a.done;
-            var result = Query.insertOrUpdate(state, entity, data, create);
-            done && done(result);
+            var entity = _a.entity, data = _a.data, create = _a.create;
+            Query.insertOrUpdate(state, entity, data, create);
         },
         /**
          * Delete data from the store.
