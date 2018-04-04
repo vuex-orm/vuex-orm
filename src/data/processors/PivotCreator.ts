5532ffe1bc2cccd848ab53669a3b60e012c9fbc5
Kia King Ishii <kia.king.08@gmail.com>
2018-04-04 23:47:01 +0900

    Fix where pivot table for the belongs to many not being created in certain case

--- src/data/processors/PivotCreator.ts
+++ src/data/processors/PivotCreator.ts
@@ -8,12 +8,12 @@ export default class PivotCreator {
    * require it for example `belongsTo` or `morphMany`.
    */
   static process (data: NormalizedData, Query: Query): NormalizedData {
-    Object.keys(data).forEach((key) => {
-      const model = Query.getModel(key)
+    Object.keys(data).forEach((entity) => {
+      const model = Query.getModel(entity)
 
       if (model.hasPivotFields()) {
         Utils.forOwn(model.pivotFields(), (field) => {
-          Utils.forOwn(field, attr => { attr.createPivots(model, data) })
+          Utils.forOwn(field, (attr, key) => { attr.createPivots(model, data, key) })
         })
       }
     })
