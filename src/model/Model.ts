c5806f4cf1459a2ed737c4a8fc5599f7e60cc8fc
Federico Dainelli <f.r.dainelli@gmail.com>
2018-05-15 21:53:12 +0200

    Refactoring

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -2,10 +2,16 @@ import BaseModel from './BaseModel'
 import Http from '../http/Http'
 import ModuleOptions from '../options/Options'
 import { Record } from '../data'
-import Query, { UpdateClosure, Condition } from '../query/Query'
+import Query, { UpdateClosure } from '../query/Query'
 import EntityCollection from '../query/EntityCollection'
 import { Collection, Item } from '../query'
-import ModelConf, { JsonModelConf , defaultConf, MethodConf, HttpMethod, PathParam } from '../model/ModelConf'
+import ModelConf, {
+  JsonModelConf,
+  defaultConf,
+  MethodConf,
+  HttpMethod,
+  PathParam
+} from '../model/ModelConf'
 import { replaceAll } from '../support/Utils'
 
 export type UpdateReturn = Item | Collection | EntityCollection
@@ -65,34 +71,27 @@ export default class Model extends BaseModel {
   }
 
   /**
-   * Get the model conf
+   * Fetch data from api server and sync to the local store (optionaly)
+   * @param {MethodConf} conf a method's conf
    * @static
-   * @return {ModelConf}
+   * @async
+   * @return {Promise<UpdateReturn>} fetched data
    */
-  public static getConf (): ModelConf {
-    return this._conf as ModelConf
-  }
+  public static async fetch (conf: MethodConf = this.getMethodConf('fetch')): Promise<Collection> {
+    const _conf = this.checkMethodConf('fetch', conf)
+    const url = this.getUrl(_conf)
+    const data = await Http[_conf.http.method as HttpMethod](url)
+      .catch((err: Error) => { console.log(err) }) || []
 
-  /**
-   * Get the method conf by name
-   * @param {string} methodName The method's name
-   * @static
-   * @return {MethodConf}
-   */
-  public static getMethodConf (methodName: string): MethodConf {
-    return this.getConf().method(methodName) as MethodConf
-  }
-
-  /**
-   * Wrap query getter
-   * @static
-   */
-  public static query (): Query {
-    return this.getters('query')()
+    if (_conf.localSync) {
+      await this.dispatch('insertOrUpdate', { data })
+    }
+    return data
   }
 
   /**
    * Wrap find method
+   * @param {MethodConf} conf a method's conf
    * @static
    * @async
    * @return {Promise<Collection>} list of results
@@ -114,10 +113,14 @@ export default class Model extends BaseModel {
   /**
    * Wrap findById method
    * @param {number} id of record to find
+   * @param {MethodConf} conf a method's conf
    * @static
    * @return {Promise<Item>} result object
    */
-  public static async findById (id: number, conf: MethodConf = this.getMethodConf('findById')): Promise<Item> {
+  public static async findById (
+    id: number,
+    conf: MethodConf = this.getMethodConf('findById')
+  ): Promise<Item> {
     const _conf = this.checkMethodConf('findById', conf)
     let data
     if (_conf.remote) {
@@ -132,99 +135,6 @@ export default class Model extends BaseModel {
   }
 
   /**
-   * Check if record identified by id param exist
-   * @param {number} id of the record to search
-   * @static
-   * @return {Promise<boolean>} the result
-   */
-  public static exist (id: number): Promise<boolean> {
-    return this.findById(id).then(
-      (item: Item) => {
-        return item !== null
-      }
-    )
-  }
-
-  /**
-   * Wrap count method
-   * @static
-   * @return {Promise<Number>} number of element
-   */
-  public static count (): Promise<number> {
-    return Promise.resolve(this.query().count())
-  }
-
-  /**
-   * Wrap deleteById method
-   * @param id of record to delete
-   * @static
-   */
-  public static deleteById (id: number): void {
-    this.dispatch('delete', id)
-  }
-
-  /**
-   * Wrap deleteAll method
-   * @static
-   */
-  public static deleteAll (): void {
-    this.dispatch('deleteAll', {})
-  }
-
-  /**
-   * Wrap create method
-   * @param {Record | Record[]} data to create
-   * @static
-   * @return {Promise<EntityCollection>} the created data
-   */
-  public static create (data: Record | Record[]): Promise<EntityCollection> {
-    return this.dispatch('create', { data })
-  }
-
-  /**
-   * Wrap update method
-   * @param {Record | Record[] | UpdateClosure} data to update
-   * @param {Condition} where conditions
-   * @static
-   * @return {Promise<UpdateReturn>} updated data
-   */
-  public static update (data: Record | Record[] | UpdateClosure, where?: Condition): Promise<UpdateReturn> {
-    return this.dispatch('update', {
-      where,
-      data
-    })
-  }
-
-  /**
-   * Wrap insertOrUpdate method
-   * @param {Record | Record[]} data to unsert or update
-   * @static
-   * @return {Promise<UpdateReturn>} data result
-   */
-  public static insertOrUpdate (data: Record | Record[]): Promise<UpdateReturn> {
-    return this.dispatch('insertOrUpdate', data)
-  }
-
-  /**
-   * Fetch data from api server and sync to the local store (optionaly)
-   * @param {MethodConf} conf a method's conf
-   * @static
-   * @async
-   * @return {Promise<UpdateReturn>} fetched data
-   */
-  public static async fetch (conf: MethodConf = this.getMethodConf('fetch')): Promise<Collection> {
-    const _conf = this.checkMethodConf('fetch', conf)
-    const url = this.getUrl(_conf)
-    const data = await Http[_conf.http.method as HttpMethod](url)
-      .catch((err: Error) => { console.log(err); }) || []
-
-    if(_conf.localSync) {
-      await this.dispatch('insertOrUpdate', { data })
-    }
-    return data
-  }
-
-  /**
    * Exec a fetchById api method with the default confs 
    * or the pass confs and sync to the local store (optionaly) 
    * @param {number} id of the fetching record
@@ -233,17 +143,193 @@ export default class Model extends BaseModel {
    * @async
    * @return {Promise<Item>} fetched item
    */
-  public static async fetchById(id: number, conf: MethodConf = this.getMethodConf('fetchById')): Promise<Item> {
+  public static async fetchById (
+    id: number, 
+    conf: MethodConf = this.getMethodConf('fetchById')
+  ): Promise<Item> {
     const _conf = this.checkMethodConf('fetchById', conf)
     const url = this.getUrl(_conf, new PathParam('id', id.toString()))
     const data = await Http[_conf.http.method as HttpMethod](url)
       .catch((err: Error) => { console.log(err); }) || []
     
     if(_conf.localSync) {
-      // await this.dispatch('insertOrUpdate', { data })
-      await this.update(data)
+      // await this.update(data)
+      await this.dispatch('insertOrUpdate', { data })
     }
     return data
+  }
+
+  /**
+   * Check if record identified by id param exist
+   * @param {number} id of the record to search
+   * @param {MethodConf} conf a method's conf
+   * @static
+   * @return {Promise<boolean>} the result
+   */
+  public static async exist (
+    id: number, 
+    conf: MethodConf = this.getMethodConf('exist')
+  ): Promise<boolean> {
+    const _conf = this.checkMethodConf('exist', conf)
+    let data
+
+    if (_conf.remote) {
+      const url = this.getUrl(_conf, new PathParam('id', id.toString()))
+      data = await Http[_conf.http.method as HttpMethod](url)
+      .catch((err: Error) => { console.log(err); }) || []
+    }
+    else {
+      data = this.query().find(id) !== null
+    }
+    return data
+  }
+
+  /**
+   * Wrap count method
+   * @param {MethodConf} conf a method's conf
+   * @static
+   * @return {Promise<Number>} number of element
+   */
+  public static async count (conf: MethodConf = this.getMethodConf('count')): Promise<number> {
+    const _conf = this.checkMethodConf('count', conf)
+    let data
+    
+    if (_conf.remote) {
+      data = await Http[_conf.http.method as HttpMethod](this.getUrl(_conf))
+      .catch((err: Error) => { console.log(err); }) || []
+    }
+    else {
+      data = this.query().count()
+    }
+    return data
+  }
+
+  /**
+   * Wrap create method
+   * @param {Record | Record[]} data to create
+   * @param {MethodConf} conf a method's conf
+   * @static
+   * @return {Promise<EntityCollection>} the created data
+   */
+  public static async create (
+    data: Record | Record[], 
+    conf: MethodConf = this.getMethodConf('create')
+  ): Promise<EntityCollection> {
+
+    const _conf = this.checkMethodConf('create', conf)
+    let dataOutput
+    if (_conf.remote) {
+      dataOutput = await Http[_conf.http.method as HttpMethod](this.getUrl(_conf), data)
+      .catch((err: Error) => { console.log(err); }) || []
+      
+      if(_conf.localSync) {
+        this.dispatch('insert', { data: dataOutput })
+      }
+    }
+    /* tslint:disable */
+    else {
+      dataOutput = this.dispatch('create', data)
+    }
+
+    return dataOutput
+  }
+
+  /**
+   * Wrap update method
+   * @param {number} id of the record to search
+   * @param {Record | Record[] | UpdateClosure} data to update
+   * @param {MethodConf} conf a method's conf
+   * @static
+   * @return {Promise<UpdateReturn>} updated data
+   */
+  public static async update (
+    id: number,
+    data: Record | Record[] | UpdateClosure,
+    conf: MethodConf = this.getMethodConf('update')
+  ): Promise<UpdateReturn> {
+
+    const _conf = this.checkMethodConf('update', conf)
+    let dataOutput
+    if (_conf.remote) {
+      const url = this.getUrl(_conf, new PathParam('id', id.toString()))
+      dataOutput = await Http[_conf.http.method as HttpMethod](url, data)
+      .catch((err: Error) => { console.log(err); }) || []
+      
+      if(_conf.localSync && dataOutput) {
+        this.dispatch('update', {
+          where: id,
+          data: dataOutput
+        })
+      }
+    }
+    /* tslint:disable */
+    else {
+      dataOutput = this.dispatch('update', {
+        where: id,
+        data
+      })
+    }
+
+    return dataOutput
+  }
+
+  /**
+   * Wrap deleteById method
+   * @param id of record to delete
+   * @param {MethodConf} conf a method's conf
+   * @static
+   */
+  public static async deleteById (
+    id: number, 
+    conf: MethodConf = this.getMethodConf('deleteById')
+  ): Promise<void> {
+
+    const _conf = this.checkMethodConf('deleteById', conf)
+
+    if (_conf.remote) {
+      const url = this.getUrl(_conf, new PathParam('id', id.toString()))
+      const dataOutput = await Http[_conf.http.method as HttpMethod](url)
+      .catch((err: Error) => { console.log(err); }) || []
+      
+      if(_conf.localSync && dataOutput) {
+        this.dispatch('delete', id)
+      }
+    }
+    /* tslint:disable */
+    else {
+      this.dispatch('delete', id)
+    }
+  }
+
+  /**
+   * Wrap deleteAll method
+   * @param {MethodConf} conf a method's conf
+   * @static
+   */
+  public static async delete (conf: MethodConf = this.getMethodConf('deleteById')): Promise<void> {
+
+    const _conf = this.checkMethodConf('deleteById', conf)
+
+    if (_conf.remote) {
+      const dataOutput = await Http[_conf.http.method as HttpMethod](this.getUrl(_conf))
+      .catch((err: Error) => { console.log(err); }) || []
+      
+      if(_conf.localSync && dataOutput) {
+        this.dispatch('deleteAll', {})
+      }
+    }
+    /* tslint:disable */
+    else {
+      this.dispatch('deleteAll', {})
+    }
+  }
+
+  /**
+   * Wrap query getter
+   * @static
+   */
+  public static query (): Query {
+    return this.getters('query')()
   }
 
   /**
@@ -254,7 +340,7 @@ export default class Model extends BaseModel {
    * @static
    * @return {string} api's url
    */
-  public static getUrl(conf: MethodConf, ...pathParams: PathParam[]): string {
+  private static getUrl (conf: MethodConf, ...pathParams: PathParam[]): string {
     let baseUrl = this._conf.baseUrl
     const methodPath = pathParams.length ? conf.http.bindPathParams(pathParams) : conf.http.path
     if(ModuleOptions.resources.baseUrl) {
@@ -274,7 +360,7 @@ export default class Model extends BaseModel {
    * @return {MethodConf} the new method's configuration
    * @throws Error
    */
-  private static checkMethodConf(methodName: string, conf: MethodConf): MethodConf {
+  private static checkMethodConf (methodName: string, conf: MethodConf): MethodConf {
     const _conf = this._conf as ModelConf
     let _method = _conf.method(methodName)
     if (conf && _method) {
@@ -285,5 +371,24 @@ export default class Model extends BaseModel {
       throw new Error(`${methodName} configuration method not found`)
     }
     return _method as MethodConf
+  }
+
+  /**
+   * Get the model conf
+   * @static
+   * @return {ModelConf}
+   */
+  private static getConf (): ModelConf {
+    return this._conf as ModelConf
+  }
+
+  /**
+   * Get the method conf by name
+   * @param {string} methodName The method's name
+   * @static
+   * @return {MethodConf}
+   */
+  private static getMethodConf (methodName: string): MethodConf {
+    return this.getConf().method(methodName) as MethodConf
   }
 }
