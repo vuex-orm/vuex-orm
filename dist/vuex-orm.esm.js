9f3353131cb307298c565a346a6f320122ad0790
Kia Ishii <kia.king.08@gmail.com>
2018-12-03 21:39:20 +0900

    [Release] 0.31.0

--- dist/vuex-orm.esm.js
+++ dist/vuex-orm.esm.js
@@ -74,12 +74,9 @@ and limitations under the License.
 ***************************************************************************** */
 /* global Reflect, Promise */
 
-var extendStatics = function(d, b) {
-    extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return extendStatics(d, b);
-};
+var extendStatics = Object.setPrototypeOf ||
+    ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+    function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
 
 function __extends(d, b) {
     extendStatics(d, b);
@@ -87,15 +84,12 @@ function __extends(d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 }
 
-var __assign = function() {
-    __assign = Object.assign || function __assign(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
+var __assign = Object.assign || function __assign(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+    }
+    return t;
 };
 
 function __awaiter(thisArg, _arguments, P, generator) {
@@ -3130,7 +3124,7 @@ var Query = /** @class */ (function () {
          */
         this.load = {};
         /**
-         * Primary key ids to filter records by
+         * Primary key ids to filter records by. Used for filtering records direct key lookup.
          */
         this._idFilter = [];
         /**
@@ -3418,7 +3412,7 @@ var Query = /** @class */ (function () {
         return this;
     };
     /**
-     * Filter records by their primary key.
+     * Filter records by their primary keys.
      */
     Query.prototype.whereIdIn = function (value) {
         this._idFilter = this._idFilter.concat(value);
