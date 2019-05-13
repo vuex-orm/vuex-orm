1ea084fbf9f6f03bec5a2634a449e564a908b117
Kia Ishii <kia.king.08@gmail.com>
2019-05-13 19:21:25 +0900

    Add test case for the issue

--- test/feature/relations/HasManyThrough.spec.js
+++ test/feature/relations/HasManyThrough.spec.js
@@ -139,6 +139,80 @@ describe('Features – Relations – Has Many Through', () => {
     expect(country.posts[2].id).toBe(2)
   })
 
+  it('can resolve has many through relationship with string value id', async () => {
+    class Country extends Model {
+      static entity = 'countries'
+
+      static fields () {
+        return {
+          id: this.string(''),
+          posts: this.hasManyThrough(Post, User, 'country_id', 'user_id')
+        }
+      }
+    }
+
+    class User extends Model {
+      static entity = 'users'
+
+      static fields () {
+        return {
+          id: this.string(''),
+          country_id: this.string(''),
+        }
+      }
+    }
+
+    class Post extends Model {
+      static entity = 'posts'
+
+      static fields () {
+        return {
+          id: this.string(''),
+          user_id: this.string(''),
+        }
+      }
+    }
+
+    const store = createStore([{ model: Country }, { model: User }, { model: Post }])
+
+    await store.dispatch('entities/users/create', {
+      data: [
+        { id: 'string-id-1', country_id: 'string-id-1' },
+        { id: 'string-id-2', country_id: 'string-id-1' },
+        { id: 'string-id-3', country_id: 'string-id-2' }
+      ]
+    })
+
+    await store.dispatch('entities/posts/create', {
+      data: [
+        { id: 'string-id-1', user_id: 'string-id-1' },
+        { id: 'string-id-2', user_id: 'string-id-2' },
+        { id: 'string-id-3', user_id: 'string-id-3' },
+        { id: 'string-id-4', user_id: 'string-id-1' }
+      ]
+    })
+
+    await store.dispatch('entities/countries/create', {
+      data: [
+        { id: 'string-id-1' },
+        { id: 'string-id-2' }
+      ]
+    })
+
+    const country = store.getters['entities/countries/query']().with('posts').find('string-id-1')
+
+    expect(country).toBeInstanceOf(Country)
+    expect(country.posts.length).toBe(3)
+    expect(country.posts[0]).toBeInstanceOf(Post)
+
+    expect(country.posts[0].user_id).toBe('string-id-1')
+    expect(country.posts[1].user_id).toBe('string-id-1')
+    expect(country.posts[2].user_id).toBe('string-id-2')
+    expect(country.posts[0].id).toBe('string-id-1')
+    expect(country.posts[1].id).toBe('string-id-4')
+    expect(country.posts[2].id).toBe('string-id-2')
+  })
+
   it('can resolve empty has many through relationship', async () => {
     class Country extends Model {
       static entity = 'countries'
