1c2eaa99c0a8571fb165572b4ea5d90713bd7b89
Szijjártó-Nagy Misu <szijjarto-nagy.mihaly@ejogseged.hu>
2019-12-09 10:59:03 +0100

    tests for registering models with modules

--- test/feature/database/RegisterModel.spec.js
+++ test/feature/database/RegisterModel.spec.js
@@ -18,6 +18,29 @@ describe('Feature – Database - Register Model', () => {
     }
   }
 
+  const userModule = {
+    state: {
+      currentId: null
+    },
+    getters: {
+      current: state => () => state.data[state.currentId]
+    },
+    mutations: {
+      setCurrent (state, id) {
+        state.currentId = id
+      }
+    },
+    actions: {
+      login ({ commit, state }, name) {
+        const id = Object.keys(state.data).find(key => state.data[key].name === name)
+        commit('setCurrent', id)
+      },
+      logout ({ commit }) {
+        commit('setCurrent', null)
+      }
+    }
+  }
+
   class Hobby extends Model {
     static entity = 'hobbies'
 
@@ -35,7 +58,7 @@ describe('Feature – Database - Register Model', () => {
     const database = new Database()
 
     database.register(User)
-    database.register(Hobby) // TODO custom module?
+    database.register(Hobby)
 
     const store = new Vuex.Store({
       plugins: [VuexORM.install(database)],
@@ -57,6 +80,34 @@ describe('Feature – Database - Register Model', () => {
     expect(hobby.name).toEqual('my hobby')
     expect(hobby.user.name).toEqual('my name')
     expect(user.name).toEqual('my name')
+  })
+
+  it('can register modules before being installed to vuex.', async () => {
+    const database = new Database()
+
+    database.register(User, userModule)
+
+    const store = new Vuex.Store({
+      plugins: [VuexORM.install(database)],
+      strict: true
+    })
+
+    await store.dispatch('entities/users/create', {
+      data: [
+        { name: 'user 1' },
+        { name: 'user 2' },
+        { name: 'user 3' }
+      ]
+    })
+
+    const user = store.getters['entities/users/query']().where('name', 'user 2').first()
+    const getCurrentUser = store.getters['entities/users/current']
+
+    expect(getCurrentUser()).toBeUndefined()
+    store.dispatch('entities/users/login', 'user 2')
+    expect(getCurrentUser()).toEqual(user)
+    store.dispatch('entities/users/logout')
+    expect(getCurrentUser()).toBeUndefined()
   })
 
   it('can register models after being installed to vuex.', async () => {
@@ -86,6 +137,35 @@ describe('Feature – Database - Register Model', () => {
     expect(hobby.name).toEqual('my hobby')
     expect(hobby.user.name).toEqual('my name')
     expect(user.name).toEqual('my name')
+  })
+
+  it('can register modules after being installed to vuex.', async () => {
+    const database = new Database()
+
+    const store = new Vuex.Store({
+      plugins: [VuexORM.install(database)],
+      strict: true
+    })
+
+    database.register(User, userModule)
+
+
+    await store.dispatch('entities/users/create', {
+      data: [
+        { name: 'user 1' },
+        { name: 'user 2' },
+        { name: 'user 3' }
+      ]
+    })
+
+    const user = store.getters['entities/users/query']().where('name', 'user 2').first()
+    const getCurrentUser = store.getters['entities/users/current']
+
+    expect(getCurrentUser()).toBeUndefined()
+    store.dispatch('entities/users/login', 'user 2')
+    expect(getCurrentUser()).toEqual(user)
+    store.dispatch('entities/users/logout')
+    expect(getCurrentUser()).toBeUndefined()
   })
 
   it('preserves state of previously registered models', async () => {
