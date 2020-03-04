950d94167688566e8faa45a5d49e75a73a2e46eb
Kia King Ishii <kia.king.08@gmail.com>
2020-03-04 17:12:00 +0900

    feat: add insert method to the repository

--- src/repository/Repository.ts
+++ src/repository/Repository.ts
@@ -1,5 +1,8 @@
 import { Store } from 'vuex'
+import Database from '../database/Database'
+import { Collections } from '../data'
 import Model from '../model/Model'
+import * as Payloads from '../modules/payloads/Actions'
 
 export default class Repository<M extends typeof Model> {
   /**
@@ -18,5 +21,34 @@ export default class Repository<M extends typeof Model> {
   constructor (store: Store<any>, model: M) {
     this.store = store
     this.model = model
+  }
+
+  /**
+   * Get the database instance from the store instance.
+   */
+  database (): Database {
+    return this.store.$db()
+  }
+
+  /**
+   * Create a namespaced method name for Vuex Module from the given
+   * method name.
+   */
+  namespace (method: string): string {
+    return `${this.database().namespace}/${this.model.entity}/${method}`
+  }
+
+  /**
+   * Dispatch Vuex Action.
+   */
+  dispatch (method: string, payload?: any): Promise<any> {
+    return this.store.dispatch(this.namespace(method), payload)
+  }
+
+  /**
+   * Insert records.
+   */
+  insert (payload: Payloads.Insert): Promise<Collections> {
+    return this.dispatch('insert', payload)
   }
 }
