20f0f4cb23b301ab48784bc6846f5149cb050148
Kia Ishii <kia.king.08@gmail.com>
2020-04-04 16:50:19 +0900

    refactor: installation related structures

---
+++ test/feature/installation/install.spec.ts
@@ -0,0 +1,74 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VuexORM, { Database, Model } from '@/index'
+
+describe('feature/installation/install', () => {
+  Vue.use(Vuex)
+
+  it('installs Vuex ORM to the store', () => {
+    class User extends Model {
+      static entity = 'users'
+    }
+    class Post extends Model {
+      static entity = 'posts'
+    }
+
+    const database = new Database()
+
+    database.register(User)
+    database.register(Post)
+
+    const store = new Vuex.Store({
+      plugins: [VuexORM.install(database)]
+    })
+
+    const expected = {
+      entities: {
+        $name: 'entities',
+        users: {
+          $name: 'users',
+          $connection: 'entities',
+          data: {}
+        },
+        posts: {
+          $name: 'posts',
+          $connection: 'entities',
+          data: {}
+        }
+      }
+    }
+
+    expect(store.state).toEqual(expected)
+  })
+
+  it('can customize the namespace', () => {
+    class User extends Model {
+      static entity = 'users'
+    }
+
+    const database = new Database()
+
+    database.register(User)
+
+    const store = new Vuex.Store({
+      plugins: [
+        VuexORM.install(database, {
+          namespace: 'database'
+        })
+      ]
+    })
+
+    const expected = {
+      database: {
+        $name: 'database',
+        users: {
+          $name: 'users',
+          $connection: 'database',
+          data: {}
+        }
+      }
+    }
+
+    expect(store.state).toEqual(expected)
+  })
+})
