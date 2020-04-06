71a8a65c48da5cf396870893e2dd691315aee08b
Kia Ishii <kia.king.08@gmail.com>
2020-04-07 02:05:49 +0900

    feat: export data and modules

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
+export * from './data/Data'
+
 import { install, Options } from './store/Store'
 import Database from './database/Database'
 import Model from './model/Model'
+import RootModule from './modules/RootModule'
+import RootState from './modules/RootState'
+import Module from './modules/Module'
+import State from './modules/State'
 
-export { install, Options, Database, Model }
+export {
+  install,
+  Options,
+  Database,
+  Model,
+  RootModule,
+  RootState,
+  Module,
+  State
+}
 
 export default {
   install,
