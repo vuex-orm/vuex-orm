c4b27bb370590157ba1b190ae05a3f006c04a73a
Emiel Bom <11626777+adm-bome@users.noreply.github.com>
2020-08-05 20:55:36 +0200

    Update BelongsToMany.ts
    Changed: Pivot attribute on a model to an active lookup on the pivot table.

--- src/attributes/relations/BelongsToMany.ts
+++ src/attributes/relations/BelongsToMany.ts
@@ -186,7 +186,13 @@ export default class BelongsToMany extends Relation {
       if (related) {
         records[id] = records[id].concat(
           related.map((model: Record) => {
-            model[this.pivotKey] = record
+            model[_this.pivotKey] = null
+            Object.defineProperty(model, this.pivotKey, {
+              get: () => {
+                return this.pivot.find([model.id, id])
+              },
+              set: (val) => { }
+            })
             return model
           })
         )
